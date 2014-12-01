4d1a5c0fd4bb52547919e7420f4ced7e005e2477
Robert Warner <rowarner@student.neumont.edu>
2014-12-01 08:35:42 -0700

    lets give that a shot

--- Source/mean/public/modules/articles/controllers/articles.client.controller.js
+++ Source/mean/public/modules/articles/controllers/articles.client.controller.js
@@ -1,9 +1,37 @@
 'use strict';
 angular.module('articles').controller('ArticlesController', function() {
-    this.abilityName = "Test";
-    this.syntax = "Syntax";
-   
-    this.generateSyntax = function generateSyntax() {
-      this.syntax = "#showtooltip" + this.abilityName;
+    this.abilitySyntax = [""];
+    this.macroSyntax = "#showtooltip";
+    
+    this.modChoices = {
+      alt: "alt",
+      shift: "ctrl",
+      control: "shift"
     };
-  });
+
+    this.generateMacroSyntax = function generateSyntax() {
+      this.macroSyntax = "#showtooltip" + "<br>";
+      for (i = 0; i < this.abilitySyntax.length; i++) { 
+        if(i>0){
+          this.macroSyntax +=  "; "
+        }
+        this.macroSyntax += abilitySyntax[i];
+      }
+    };
+
+    this.addAbility = function() {
+      this.abilitySyntax.push('');
+    };
+
+    this.cast = function cast(abilityName) {
+      return "/cast " + abilityName;
+    };
+    this.castWithMod = function castWithMod(abilityName, modkey) {
+      return "/cast [mod: " + modkey + "] " + abilityName;
+    };
+
+  }).filter('iif', function () {
+   return function(input, trueValue, falseValue) {
+        return input ? trueValue : falseValue;
+   };
+});;
