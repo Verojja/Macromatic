40f6f12894b426faf01273d7a04c2a4cb6d97893
Robert Warner <rowarner@student.neumont.edu>
2014-12-01 07:06:17 -0700

    Testing new page

---
+++ Source/mean/public/modules/macros/config/macros.routes.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Setting up route
+angular.module('macros').config(['$stateProvider',
+	function($stateProvider) {
+		// Articles state routing
+		$stateProvider.
+		state('listArticles', {
+			url: '/articles',
+			templateUrl: 'modules/articles/views/list-articles.client.view.html'
+		}).
+		state('createArticle', {
+			url: '/macros/create',
+			templateUrl: 'modules/macros/views/macros.create.html'
+		}).
+		state('viewArticle', {
+			url: '/articles/:articleId',
+			templateUrl: 'modules/articles/views/view-article.client.view.html'
+		}).
+		state('editArticle', {
+			url: '/articles/:articleId/edit',
+			templateUrl: 'modules/articles/views/edit-article.client.view.html'
+		});
+	}
+]);
